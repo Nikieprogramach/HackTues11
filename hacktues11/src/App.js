2a98cb489a566cd92ee1b5f99a9b94f0991b3068
GabrielPetrov <gabriel.milen.petrov@gmail.com>
2025-03-20 14:03:24 +0200

    Basic searchbar

--- hacktues11/src/App.js
+++ hacktues11/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [hasSearched, setHasSearched] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = () => {
+    if (searchQuery.trim()) {
+      // Perform search logic here
+      setHasSearched(true);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className={`search-bar ${hasSearched ? 'search-bar-top' : 'search-bar-center'}`}>
+        <input
+          type="text"
+          placeholder="Type business name here"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              handleSearch(e.target.value);
+            }
+          }}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      {hasSearched && (
+        <div className="content">
+          <p>searched</p>
+        </div>
+      )}
     </div>
   );
 }
